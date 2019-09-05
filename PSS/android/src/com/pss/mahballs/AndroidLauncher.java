302dcbe32465cf84453d206939a156cec84d328c
kyaz123 <30334728+kyaz123@users.noreply.github.com>
2019-09-05 13:50:02 -0600

    Add files via upload

---
+++ PSS/android/src/com/pss/mahballs/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pss.mahballs;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pss.mahballs.PSS;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PSS(), config);
+	}
+}
